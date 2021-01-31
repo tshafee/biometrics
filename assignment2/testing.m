9292382ef148b6b7bf14f7e5bc8030531a198998
Jacco Brandt <djekkoo@hotmail.com>
2021-01-31 23:23:50 +0100

    plot ROC graph @test

--- assignment2/testing.m
+++ assignment2/testing.m
@@ -1,4 +1,4 @@
-function [S] = testing
+function [] = testing
 
 % load test image set
 [Xtr, Xte, pixel_vector_test_identities] = load_images;
@@ -30,4 +30,52 @@ for el_1 = 1:l
         S(el_1, el_2) = sqrt(sum((v1-v2) .^ 2));
     end
 end
+
+
+% calculate ROC
+false_match_rate = [];
+false_non_match_rate = [];
+true_match_rate = []
+thresholds = [];
+for t = 0:0.2:10
+    thresholds = [thresholds, t];
+    % compute match rate for threshold i
+    count_of_checks_match = 0;
+    count_of_checks_non_match = 0;
+    count_of_FM = 0;
+    count_of_FNM = 0;
+    
+    for i = 1:l
+        % get identity for this index
+        temp_id = pixel_vector_test_identities(i);
+        for j = (i+1):l
+            compare_id = pixel_vector_test_identities(j);
+            if temp_id == compare_id
+                %same person
+                count_of_checks_match = count_of_checks_match + 1;
+                if S(i, j) >= t
+                   count_of_FNM = count_of_FNM + 1; 
+                end
+            else
+                %not same person
+                count_of_checks_non_match = count_of_checks_non_match + 1;
+                if S(i, j) < t
+                    count_of_FM = count_of_FM + 1;
+                end
+            end
+        end
+    end
+    
+    fmr = double(count_of_FM/count_of_checks_non_match);
+    fnmr = double(count_of_FNM/count_of_checks_match);
+    
+    false_match_rate = [false_match_rate, fmr];
+    false_non_match_rate = [false_non_match_rate, fnmr];
+    true_match_rate = [true_match_rate, double(1-fnmr)];
+end
+
+
+figure(1); % ROC
+plot(false_match_rate, true_match_rate)
+xlabel('False match rate FMR(t)'); ylabel('True match rate TMR(t)'); title('Receiver operating characteristic (ROC)');
 
